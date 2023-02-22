47a52d8bac2640f6f8efc20ac7ac0748fd7a9926
Maksym Stelmakh <stelmakh1994@gmail.com>
2023-02-22 12:35:25 +0200

    add unique filename

--- controllers/user/updateAvatar.js
+++ controllers/user/updateAvatar.js
@@ -7,10 +7,15 @@ const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 const updateAvatar = async (req, res, next) => {
 	const { path: tempUpload, originalname } = req.file;
 	const { id } = req.user;
+	console.log(req.user);
 	try {
-		const resultUpload = path.join(avatarsDir, originalname);
+		const resultUpload = path.join(avatarsDir, `${id}${originalname}`);
 		await fs.rename(tempUpload, resultUpload);
-		const avatarURL = path.join("public", "avatars", originalname);
+		const avatarURL = path.join(
+			"public",
+			"avatars",
+			`${id}${originalname}`
+		);
 		await User.findByIdAndUpdate(id, { avatarURL });
 		res.status(200).json(avatarURL);
 	} catch (error) {
